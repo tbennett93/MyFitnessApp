d5e9c92cc176db94a6bf86917390527d7054e33b
tbenn <tbennett@hotmail.co.uk>
2020-06-15 00:05:16 +0100

    app setup with new orange and black colour scheme. started to make progress towards data entry on the Goals page

--- app/src/main/java/com/example/myfitnessappv4/ui/goals/GoalsFragment.java
+++ app/src/main/java/com/example/myfitnessappv4/ui/goals/GoalsFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import androidx.lifecycle.ViewModelProviders;
@@ -16,8 +18,8 @@ import com.example.myfitnessappv4.R;
 public class GoalsFragment extends Fragment {
 
     private com.example.myfitnessappv4.ui.goals.GoalsViewModel goalsViewModel;
-    private TextView appendCurrentWeight;
-
+    private LinearLayout clickableCurrentWeight;
+    private TextView enterCurrentWeight;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -27,13 +29,21 @@ public class GoalsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_goals, container, false);
 
 
-        appendCurrentWeight = (TextView) root.findViewById(R.id.enterCurrentBodyfat);
+        //Enter current weight
+        enterCurrentWeight = (TextView) root.findViewById(R.id.enterCurrentWeight);
+        clickableCurrentWeight = (LinearLayout) root.findViewById(R.id.currentWeightLayout);
+        clickableCurrentWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO have this popup with a data entry screen (create new xml layout file, horiontal linear layout)
+                enterCurrentWeight.setText("testing");
+
+            }
+        });
 
 
-//        final TextView addunit = (TextView) root.findViewById(R.id.enterWeightUnit);
-//
-//        final String suffix = "lbs"; //Change! this so then it looks at whether lb or kg is setup
-//        addunit.setText(suffix);
+        //Hides the title of the app
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
 
         return root;
 
