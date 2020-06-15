f6a8ef3a8e199493ab5122cd42845cf444a780e4
tbenn <tbennett@hotmail.co.uk>
2020-06-15 22:20:29 +0100

    app setup with new orange and black colour scheme. started to make progress towards data entry on the Goals page

--- app/src/main/java/com/example/myfitnessappv4/ui/goals/GoalsFragment.java
+++ app/src/main/java/com/example/myfitnessappv4/ui/goals/GoalsFragment.java
@@ -1,10 +1,18 @@
 package com.example.myfitnessappv4.ui.goals;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,32 +21,57 @@ import androidx.fragment.app.Fragment;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.myfitnessappv4.MainActivity;
 import com.example.myfitnessappv4.R;
 
 public class GoalsFragment extends Fragment {
 
-    private com.example.myfitnessappv4.ui.goals.GoalsViewModel goalsViewModel;
+//    private com.example.myfitnessappv4.ui.goals.GoalsViewModel goalsViewModel;
     private LinearLayout clickableCurrentWeight;
     private TextView enterCurrentWeight;
+    Point p;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        goalsViewModel =
-                ViewModelProviders.of(this).get(com.example.myfitnessappv4.ui.goals.GoalsViewModel.class); //doesn't seem to behave anay differently than = new GoalsViewModel
-        View root = inflater.inflate(R.layout.fragment_goals, container, false);
+//        goalsViewModel =
+//                ViewModelProviders.of(this).get(com.example.myfitnessappv4.ui.goals.GoalsViewModel.class); //doesn't seem to behave anay differently than = new GoalsViewModel
+
+        final View root = inflater.inflate(R.layout.fragment_goals, container, false);
+
+
 
 
         //Enter current weight
         enterCurrentWeight = (TextView) root.findViewById(R.id.enterCurrentWeight);
         clickableCurrentWeight = (LinearLayout) root.findViewById(R.id.currentWeightLayout);
+
+
+
         clickableCurrentWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO have this popup with a data entry screen (create new xml layout file, horiontal linear layout)
-                enterCurrentWeight.setText("testing");
+
+
+
+                View popupView = LayoutInflater.from(getActivity()).inflate(R.layout.popup_enter_weight, null);
+
+
+//                LinearLayout popupEnterWeightLayout = root.findViewById(R.id.popupEnterWeightLayout);
+
+                enterCurrentWeight.setText(popupView.getWidth() + "and" + popupView.getHeight() );
+
+                final PopupWindow popupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                popupWindow.setAnimationStyle(R.style.Animation_Design_BottomSheetDialog);
+                popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+                dimBehind(popupWindow);
+
 
             }
+
+
         });
 
 
@@ -50,4 +83,18 @@ public class GoalsFragment extends Fragment {
 
     }
 
+
+//TODO understand this
+    public static void dimBehind(PopupWindow popupWindow) {
+        View container = popupWindow.getContentView().getRootView();
+        Context context = popupWindow.getContentView().getContext();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+        p.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        p.dimAmount = 0.6f;
+        wm.updateViewLayout(container, p);
+    }
+
+
 }
+
